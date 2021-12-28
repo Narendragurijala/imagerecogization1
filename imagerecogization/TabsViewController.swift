c858b028a2a11a51790bb1524de9a4c71485b148
Narendragurijala <mobilenarendraios@gmail.com>
2021-12-28 14:16:01 +0530

    1d

---
+++ imagerecogization/TabsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TabsViewController.swift
+//  imagerecogization
+//
+//  Created by Rahul Singh on 28/12/21.
+//
+
+import UIKit
+
+class TabsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+
+
+}
